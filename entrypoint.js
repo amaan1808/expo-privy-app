c8b56d92c66ca4bcf24423fdc1e1139bae5c638b
Joshua Navi <josh@privy.io>
2024-10-07 13:06:17 -0400

    fix

--- entrypoint.js
+++ entrypoint.js
@@ -1,8 +1,20 @@
 // entrypoint.js
 
 // Import required polyfills first
-import 'fast-text-encoding';
-import 'react-native-get-random-values';
-import '@ethersproject/shims';
+import "fast-text-encoding";
+import "react-native-get-random-values";
+import "@ethersproject/shims";
 // Then import the expo router
-import 'expo-router/entry';
+
+const _atob = atob;
+
+atob = (val) => {
+  try {
+    return _atob(val);
+  } catch (e) {
+    console.warn(e);
+    return _atob(val + "=");
+  }
+};
+
+import "expo-router/entry";
--- metro.config.js
+++ metro.config.js
@@ -6,9 +6,8 @@
 
 // module.exports = config;
 
-
-const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require("expo/metro-config");
 
 // Get the Sentry configuration
 const sentryConfig = getSentryExpoConfig(__dirname);
@@ -16,20 +15,20 @@ const sentryConfig = getSentryExpoConfig(__dirname);
 // Get the default Expo configuration
 const expoConfig = getDefaultConfig(__dirname);
 
-
 // Merge the configurations
 const combinedConfig = {
   ...sentryConfig,
+  ...expoConfig,
   transformer: {
-      ...sentryConfig.transformer,
-      ...expoConfig.transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+    ...sentryConfig.transformer,
+    ...expoConfig.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
   },
   resolver: {
     ...sentryConfig.resolver,
     ...expoConfig.resolver,
-    assetExts: sentryConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sentryConfig.resolver.sourceExts, 'svg'],
+    assetExts: sentryConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sentryConfig.resolver.sourceExts, "svg"],
   },
 };
 
